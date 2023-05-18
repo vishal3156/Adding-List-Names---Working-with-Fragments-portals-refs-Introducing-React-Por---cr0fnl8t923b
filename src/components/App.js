ce003a904be9dcbf7ac24d744dedeebab8d610d3
Vishal Suman <119152809+vishal3156@users.noreply.github.com>
2023-05-18 11:26:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
-import React , {useState,useEffect} from 'react'
+import React , {useState,useEffect, useRef} from 'react'
 import '../styles/App.css';
 import List from "./List";
 const App = () => {
   //code here
+  const inpref = useRef(null);
+  const [list, setList] = useState([]);
+  const addVal = () => {
+    setList((prev)=>[...prev,inpref.current.value]);
+  }
   return (
     <div id="main">
-       <input id="input" onChange={} />
-      <button id="button" onClick={}>Click</button>
+       <input id="input" ref={inpref} />
+      <button id="button" onClick={addVal}>Click</button>
        <ul id="list">
       <List listx={list} />
       </ul>
